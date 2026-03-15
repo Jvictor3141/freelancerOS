b0aa29d8b4cf713d04d9bd14cc80418f1744aed8
Jvictor3141 <jvcajado2.0@gmail.com>
2026-03-14 21:52:07 -0300

    inicio da incrementacao com supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,16 +1,16 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// if (!supabaseUrl || !supabaseAnonKey) {
-//   throw new Error('Supabase env vars ausentes.');
-// }
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase env vars ausentes.');
+}
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-//   auth: {
-//     persistSession: true,
-//     autoRefreshToken: true,
-//     detectSessionInUrl: true,
-//   },
-// });
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
